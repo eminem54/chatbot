377a73a6ab27f2f7ffbbbbf02079e6194b28c3a4
EasyZero <eminem54@naver.com>
2019-03-25 00:22:29 +0900

    JY extract address

--- entity_extractor.py
+++ entity_extractor.py
@@ -1,5 +1,5 @@
 import pymongo
-
+import re
 
 def get_entity(line, entity_list, num):
     connection = pymongo.MongoClient("localhost", 27017)
@@ -12,3 +12,24 @@ def get_entity(line, entity_list, num):
             line = line.replace(entity, entity_class[0])
             entity_list.append(entity)
     return line, entity_list
+
+def get_location(line, entity_list):
+    connection = pymongo.MongoClient("localhost", 27017)
+    db = connection.testDB
+    collection_name = "address"
+    co = db[collection_name]
+    texts = line.split(' ')
+
+    many_answers = []
+    for entity in co.distinct("address"):
+        for voca in texts:
+            if voca in entity:
+                many_answers.append(entity)
+
+    if len(many_answers) > 1:
+        return many_answers[0].split(' ')[2]
+    else:
+        return many_answers[0]
+
+
+#print(get_location("광진구 금고 위치", []))
--- main.py
+++ main.py
@@ -39,10 +39,10 @@ def server_msg_function(msg):
     room=session.get('room')
     slotfilling=False
 
-    intentData, _, _, _, _=chatbot.run(msg)
+    intentData, _= chatbot.run(msg)
 
     branch_information=False
-    intentData, _, _, _, _ = chatbot.run(msg)
+    intentData, _= chatbot.run(msg)
 
     #모델 돌려서 슬롯필링으로 처리할지 그냥 넘길지 판단 후
     if slotfilling==False or branch_information==False:
