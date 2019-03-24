3ee27fd2fcbbcc29ee6bb7197b9d06c66419a712
jokerKwu <35329247+jokerKwu@users.noreply.github.com>
2019-03-24 21:55:32 +0900

    multi chatting room update
    고객이 접속하게 되면 고유 세션값을 가지도록 구현해서 각각 고객들에 요청에 대답할 수 있도록 수정하였습니다.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, render_template,session
-from flask_socketio import SocketIO
+from flask import Flask, render_template,session,session,redirect,url_for,request
+from flask_socketio import SocketIO,emit,join_room,leave_room
 import logging
 import chatbot_model as re
 
@@ -12,40 +12,45 @@ socketio = SocketIO(app)
 #html 호출전 초기화
 @app.before_request
 def before_request():
-    if 'session' in session and 'user-id' in session:
-        pass
-    else:
-        session['session']=os.urandom(24)
-        session['username']='고객'
+    session['room']=os.urandom(24)
+    session['name']='고객'
 
 #초기 화면 연결
 @app.route("/",methods=['GET','POST'])
 def index():
-    return render_template('chat.html')
+    name=session.get('name','')
+    room=session.get('room','')
+    return render_template('chat.html',name=name,room=room)
 
+
+#서버 클라이언트 연결
 #연결되면 전송되는 메시지
-@socketio.on('connect')
-def initMsg():
-    socketio.emit('init',{'data':'새마을금고 고객센터에 오신것을 환영합니다.'})
+@socketio.on('joined')
+def joined(msg):
+    room=session.get('room','')
+    join_room(room)
+    socketio.emit('init',{'data':'새마을금고 고객센터에 오신것을 환영합니다.'},room=room)
+
 
 #고객으로부터 메시지를 받으면 처리 후 다시 고객에게 메시지 전달
 @socketio.on('serverMsg')
 def server_msg_function(msg):
     print('client: ' + msg)
+    room=session.get('room')
     slotfilling=False
     branch_information=False
     intentData, _, _, _, _ = chatbot.run(msg)
     #모델 돌려서 슬롯필링으로 처리할지 그냥 넘길지 판단 후
     if slotfilling==False or branch_information==False:
-        socketio.emit('messageClient',{'data':msg})
-        socketio.emit('messageServer',{'data':intentData})
+        socketio.emit('messageClient',{'data':msg},room=room)
+        socketio.emit('messageServer',{'data':intentData},room=room)
     elif slotfilling==True and branch_information==False:
-        socketio.emit('messageClient',{'data':msg})
+        socketio.emit('messageClient',{'data':msg},room=room)
         arr=['대출','이자','상품','기타']
-        socketio.emit('slot',{'data':arr})
+        socketio.emit('slot',{'data':arr},room=room)
     if branch_information==True and slotfilling==False:
-        socketio.emit('messageClient',{'data':msg})
-        socketio.emit('messageServerLocation',{'data':intentData})
+        socketio.emit('messageClient',{'data':msg},room=room)
+        socketio.emit('messageServerLocation',{'data':intentData},room=room)
 
 if __name__ == '__main__':
     mylogger=logging.getLogger("새마을금고")
