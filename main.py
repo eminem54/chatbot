df4f8d2b2a49372a7610f882b5a58d860b042e47
jokerKwu <pkjhj485@naver.com>
2019-03-31 22:28:12 +0900

    pdf download update

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template,session,session,redirect,url_for,reques
 from flask_socketio import SocketIO,emit,join_room,leave_room
 import logging
 import chatbot_model as re
+import gridfs
+import pymongo
 
 
 chatbot=re.ChatBot()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
+connection = pymongo.MongoClient()
 #html 호출전 초기화
 @app.before_request
 def before_request():
@@ -23,6 +26,20 @@ def index():
     return render_template('chat.html',name=name,room=room)
 
 
+#pdf 다운
+@app.route('/download/<name>')
+def downloadFile(name=None):
+    fs = gridfs.GridFS(connection.Product)
+
+    if name is not None:
+        thing = fs.get_last_version(name)
+
+        r = app.response_class(thing, direct_passthrough=True, mimetype='application/octet-stream')
+
+        r.headers.set('Content-Disposition', 'attachment', filename=name)
+
+        return r
+
 #서버 클라이언트 연결
 #연결되면 전송되는 메시지
 @socketio.on('joined')
@@ -35,15 +52,20 @@ def joined(msg):
 #고객으로부터 메시지를 받으면 처리 후 다시 고객에게 메시지 전달
 @socketio.on('serverMsg')
 def server_msg_function(msg):
-    print('client: ' + msg)
     room=session.get('room')
-    slotfilling=True
+    slotfilling=False           #버튼 생성
+    branch_information=True     #지도 생성
+    pdf_download_check=False    #pdf 다운 생성
 
 
-    branch_information=False
     intentData, _= chatbot.run(msg)
 
     #모델 돌려서 슬롯필링으로 처리할지 그냥 넘길지 판단 후
+
+    ##클라이언트에 메시지 보낼 때 클라이언트 메시지 먼저 전송 후 서버 메시지 전송
+    #db 조회후 상품에 관련된 url 주소를 넘겨주면 된다.
+
+    #슬롯필링 버튼 여러개 생성하는 조건문
     if slotfilling==False and branch_information==False:
         socketio.emit('messageClient',{'data':msg},room=room)
         socketio.emit('messageServer',{'data':intentData},room=room)
@@ -51,9 +73,15 @@ def server_msg_function(msg):
         socketio.emit('messageClient',{'data':msg},room=room)
         arr=['대출','이자','상품','기타']
         socketio.emit('slot',{'data':arr},room=room)
+
+    #지점안내 지도 그려지는 조건문 (키워드가 주어지면 그 키워드에 맞는 지도를 출력해주면 된다.)
     if branch_information==True and slotfilling==False:
         socketio.emit('messageClient',{'data':msg},room=room)
         socketio.emit('messageServerLocation',{'data':intentData},room=room)
+
+    #pdf 약관 다운 받는 조건문
+    if pdf_download_check==True:
+        socketio.emit('pdf_download',{'data':msg},room=room)
 
 if __name__ == '__main__':
     mylogger=logging.getLogger("새마을금고")
