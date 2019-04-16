da9e30560d6c0fe38c443c1ae42f1fedc90477b7
jokerKwu <pkjhj485@naver.com>
2019-04-16 13:56:22 +0900

    faq 작업 중

--- main.py
+++ main.py
@@ -58,37 +58,55 @@ def server_msg_function(msg):
     pdf_download_check=False    #pdf 다운 생성
     ##todo 시나리오: 기본적으로 버튼을 생성 슬롯필링중엔 버튼 생성 멈춤.
 
-    intentData, slot= chatbot.run(msg)
-    print("지점안내 : "+intentData)
+    faq_check=msg[:9]
+    print('문자열 확인 '+faq_check)
+    if faq_check=='자주 묻는 키워드':
+        if len(msg)!=9:
+            select_faq=msg[10:]
+            if select_faq=='뒤로가기':
+                socketio.emit('messageClient',{'data':select_faq},room=room)
+                socketio.emit('faq_slot', {'data': '원하시는 카테고리를 선택해주세요.', 'slots': ['전체보기', '예적금', '대출', '인터넷뱅킹', '기타']},
+                              room=room)
+            elif select_faq=='전체보기':
+                socketio.emit('messageClient',{'data':select_faq},room=room)
+                #해당되는 질문 & 답을 전부 출력하면 된다.
 
-    if intentData[-5:] == '새마을금고':
-        branch_information = True
+        else:
+            socketio.emit('messageClient', {'data': msg}, room=room)
+            socketio.emit('faq_slot', {'data': '원하시는 카테고리를 선택해주세요.', 'slots': ['전체보기', '예적금', '대출', '인터넷뱅킹', '기타']},
+                          room=room)
 
-    #모델 돌려서 슬롯필링으로 처리할지 그냥 넘길지 판단 후
+    ################## 모델 돌린다.
+    else :
+        intentData, slot= chatbot.run(msg)
+        print("의도 확인 : "+intentData)
 
-    ##클라이언트에 메시지 보낼 때 클라이언트 메시지 먼저 전송 후 서버 메시지 전송
-    #db 조회후 상품에 관련된 url 주소를 넘겨주면 된다.
-    #기본 메시지 전달
-    if slotfilling==False and branch_information==False:
-        socketio.emit('messageClient',{'data':msg},room=room)
-        socketio.emit('messageServer',{'data':intentData},room=room)
+        if intentData[-5:] == '새마을금고':
+            branch_information = True
 
-    # 슬롯필링 버튼 여러개 생성하는 조건문
-    elif slotfilling==True and branch_information==False:
-        socketio.emit('messageClient',{'data':msg},room=room)
+        ##클라이언트에 메시지 보낼 때 클라이언트 메시지 먼저 전송 후 서버 메시지 전송
+        #db 조회후 상품에 관련된 url 주소를 넘겨주면 된다.
+        #기본 메시지 전달
+        if slotfilling==False and branch_information==False:
+            socketio.emit('messageClient',{'data':msg},room=room)
+            socketio.emit('messageServer',{'data':intentData},room=room)
 
-        arr=['상품안내', '지점안내', 'FAQ']
-        socketio.emit('slot',{'data':'아래 항목 중에서 선택해주세요.','slots':arr},room=room)
+        # 슬롯필링 버튼 여러개 생성하는 조건문
+        elif slotfilling==True and branch_information==False:
+            socketio.emit('messageClient',{'data':msg},room=room)
 
-    #지점안내 지도 그려지는 조건문 (키워드가 주어지면 그 키워드에 맞는 지도를 출력해주면 된다.)
+            arr=['상품안내', '지점안내', 'FAQ']
+            socketio.emit('slot',{'data':'아래 항목 중에서 선택해주세요.','slots':arr},room=room)
 
-    if branch_information==True and slotfilling==False:
-        socketio.emit('messageClient',{'data':msg},room=room)
-        socketio.emit('messageServerLocation',{'data':intentData},room=room)
+        #지점안내 지도 그려지는 조건문 (키워드가 주어지면 그 키워드에 맞는 지도를 출력해주면 된다.)
 
-    #pdf 약관 다운 받는 조건문
-    if pdf_download_check==True:
-        socketio.emit('pdf_download',{'data':msg},room=room)
+        if branch_information==True and slotfilling==False:
+            socketio.emit('messageClient',{'data':msg},room=room)
+            socketio.emit('messageServerLocation',{'data':intentData},room=room)
+
+        #pdf 약관 다운 받는 조건문
+        if pdf_download_check==True:
+            socketio.emit('pdf_download',{'data':msg},room=room)
 
 if __name__ == '__main__':
     mylogger=logging.getLogger("새마을금고")
