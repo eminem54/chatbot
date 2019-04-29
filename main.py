6fbc0c437898940e2d3dbbb5fd0b4a5ee246f102
eminem54 <eminem54@naver.com>
2019-04-29 13:00:05 +0900

    i'don't know

--- main.py
+++ main.py
@@ -53,42 +53,25 @@ def joined(msg):
 def server_msg_function(msg):
     room=session.get('room')
 
-    slotfilling=False         #버튼 생성
-    branch_information=False     #지도 생성
-    pdf_download_check=False    #pdf 다운 생성
-    ##todo 시나리오: 기본적으로 버튼을 생성 슬롯필링중엔 버튼 생성 멈춤.
 
-    intentData, _= chatbot.run(msg)
-    print("지점안내 : "+intentData)
 
-    if intentData[-5:] == '새마을금고':
-        branch_information = True
+    intentData, slot= chatbot.run(msg)
+    print("지점안내 : "+intentData, slot.intent)
+
+    if slot.intent == '지점 안내':
+        socketio.emit('messageClient',{'data':msg},room=room)
+        socketio.emit('messageServerLocation',{'data':intentData},room=room)
+
+    elif slot.intent == '상품 소개':
+        socketio.emit('messageClient',{'data':msg},room=room)
+        socketio.emit('messageServer',{'data':intentData},room=room)
+
 
     #모델 돌려서 슬롯필링으로 처리할지 그냥 넘길지 판단 후
 
     ##클라이언트에 메시지 보낼 때 클라이언트 메시지 먼저 전송 후 서버 메시지 전송
     #db 조회후 상품에 관련된 url 주소를 넘겨주면 된다.
     #기본 메시지 전달
-    if slotfilling==False and branch_information==False:
-        socketio.emit('messageClient',{'data':msg},room=room)
-        socketio.emit('messageServer',{'data':intentData},room=room)
-
-    # 슬롯필링 버튼 여러개 생성하는 조건문
-    elif slotfilling==True and branch_information==False:
-        socketio.emit('messageClient',{'data':msg},room=room)
-
-        arr=['상품안내', '지점안내', 'FAQ']
-        socketio.emit('slot',{'data':'아래 항목 중에서 선택해주세요.','slots':arr},room=room)
-
-    #지점안내 지도 그려지는 조건문 (키워드가 주어지면 그 키워드에 맞는 지도를 출력해주면 된다.)
-
-    if branch_information==True and slotfilling==False:
-        socketio.emit('messageClient',{'data':msg},room=room)
-        socketio.emit('messageServerLocation',{'data':intentData},room=room)
-
-    #pdf 약관 다운 받는 조건문
-    if pdf_download_check==True:
-        socketio.emit('pdf_download',{'data':msg},room=room)
 
 if __name__ == '__main__':
     mylogger=logging.getLogger("새마을금고")
